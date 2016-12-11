53f8a3214abca06ea3ba3f968c9a72a53de9ddc6
ccisgood <chenchengynx@gmail.com>
2016-12-11 14:28:52 -0500

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'Ahp8HL6okfKsJe2mlgMSaYtvwg-LwgPufSSS4Jk2DuAp2_uTs7dX-X8ylHGGm_bU'
+    });
+}
