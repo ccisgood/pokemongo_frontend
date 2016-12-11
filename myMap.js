029bcc75265ef6fd31192563c635b4a1dc1893f1
ccisgood <chenchengynx@gmail.com>
2016-12-11 14:33:48 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
-var map;
-function loadMapScenario() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Ahp8HL6okfKsJe2mlgMSaYtvwg-LwgPufSSS4Jk2DuAp2_uTs7dX-X8ylHGGm_bU'
-    });
-}
+var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+    credentials: 'Ahp8HL6okfKsJe2mlgMSaYtvwg-LwgPufSSS4Jk2DuAp2_uTs7dX-X8ylHGGm_bU'
+});
+var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+    anchor: new Microsoft.Maps.Point(12, 39) });
+map.entities.push(pushpin);
