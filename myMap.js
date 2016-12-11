e2b03fe4129449b155f6c4882027c90d359cdb07
ccisgood <chenchengynx@gmail.com>
2016-12-11 15:47:43 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,8 +1,33 @@
+var map_manager = {
+    "map" : null,
+    "map_items" : []
+}
+
+map_manager.map_items = [
+    {
+        "pokemon_id" : 12,
+        "expire" : 1480822665,
+        "longitude" : -73.45,
+        "latitude" : 40.75
+    },
+    {
+        "pokemon_id" : 2,
+        "expire" : 1480822665,
+        "longitude" : -73.48,
+        "latitude" : 40.75
+    }
+]
+
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'Ahp8HL6okfKsJe2mlgMSaYtvwg-LwgPufSSS4Jk2DuAp2_uTs7dX-X8ylHGGm_bU'
     });
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
-        anchor: new Microsoft.Maps.Point(12, 39) });
-    map.entities.push(pushpin);
+    map_namager.map = map;
+    
+    for (var i in map_manager.map_items) {
+        var map_item = map_manager.map_items[i];
+        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(latitude: map_item["latitude"], longitude: map_item["longitude"]), 
+                                                 { icon: 'https://github.com/ccisgood/pokemongo_image/tree/gh-pages/images/pushpin_images/pokemon/' + map_item["pokemon_id"] + '.png'});
+        map.entities.push(pushpin);
+    }
 }
